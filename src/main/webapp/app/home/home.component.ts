6720cb1cdd76ddc43e84d3c99dc3ce093a89ffeb
MAEL <mael.petit@wanadoo.fr>
2017-11-12 14:18:37 +0100

    home.component.ts

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -3,6 +3,13 @@ import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 
 import { Account, LoginModalService, Principal } from '../shared';
+import {Person} from '../entities/person/person.model';
+import {PersonService} from '../entities/person/person.service';
+import {Sport} from '../entities/sport/sport.model';
+import {PlaceService} from '../entities/place/place.service';
+import {SportService} from '../entities/sport/sport.service';
+import {Place} from '../entities/place/place.model';
+import {ResponseWrapper} from "../shared/model/response-wrapper.model";
 
 @Component({
     selector: 'jhi-home',
@@ -15,19 +22,48 @@ import { Account, LoginModalService, Principal } from '../shared';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    person: Person;
+    sports: MySport[];
+    places: Place[];
 
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+        private personService: PersonService,
+        private sportService: SportService,
+        private placeService: PlaceService
     ) {
+        this.sports = [];
     }
 
     ngOnInit() {
         this.principal.identity().then((account) => {
             this.account = account;
+            this.personService.findByLogin(this.account.login).subscribe((person) => {
+               this.person = person;
+               console.log(this.person);
+               this.loadSportsAndPlaces();
+            });
+
         });
         this.registerAuthenticationSuccess();
+
+    }
+
+    loadSportsAndPlaces(){
+        this.sportService.query().subscribe((res: ResponseWrapper) => {
+            for(var sport of res.json){
+                var mySport = new MySport();
+                mySport.sport = sport;
+                mySport.checked = this.person.containsSport(mySport.sport.id);
+                this.sports.push(mySport);
+            }
+        });
+
+        this.placeService.query().subscribe((res: ResponseWrapper) => {
+            this.places = res.json;
+        });
     }
 
     registerAuthenticationSuccess() {
@@ -45,4 +81,13 @@ export class HomeComponent implements OnInit {
     login() {
         this.modalRef = this.loginModalService.open();
     }
+
+    go(){
+
+    }
+}
+
+class MySport{
+    sport: Sport;
+    checked: boolean;
 }
